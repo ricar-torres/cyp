a7189cf5d73d1b68a9b58a6a69a4e9d2b316ca03
RacirTorres <cs.torresricardo@gmail.com>
2020-08-21 13:07:55 -0400

    added rules to tutor

--- client-admin/src/app/shared/client-wizard.service.ts
+++ client-admin/src/app/shared/client-wizard.service.ts
@@ -71,8 +71,20 @@ export class ClientWizardService {
   tutorInformation = this.formBuilder.group({
     Id: [''],
     ClientId: [''],
-    Name: [''],
-    LastName: [''],
+    Name: [
+      '',
+      [
+        Validators.pattern('^[A-Za-z\u00C0-\u00FF]*$'),
+        Validators.maxLength(250),
+      ],
+    ],
+    LastName: [
+      '',
+      [
+        Validators.pattern('^[A-Za-z\u00C0-\u00FF]*$'),
+        Validators.maxLength(250),
+      ],
+    ],
     Phone: [''],
   });
 
@@ -188,7 +200,7 @@ export class ClientWizardService {
   private addValues() {
     var buff = Object.assign(
       this.clientDemographic.getRawValue(),
-      this.generalInformationForm.value
+      this.generalInformationForm.getRawValue()
     );
     buff['Tutors'] = [this.tutorInformation.value];
     // console.log(buff);
