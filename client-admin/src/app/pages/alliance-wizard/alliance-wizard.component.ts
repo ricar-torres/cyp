65e7647ec6303365f9bc749b7435875d346f9253
Emanuel Silva Cotto <siilva@MacBook-Pro.local>
2020-08-25 18:09:36 -0400

    stepper and first controls

--- client-admin/src/app/pages/alliance-wizard/alliance-wizard.component.ts
+++ client-admin/src/app/pages/alliance-wizard/alliance-wizard.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { QualifyingEventService } from '@app/shared/qualifying-event.service';
 
 @Component({
   selector: 'app-alliance-wizard',
   templateUrl: './alliance-wizard.component.html',
-  styleUrls: ['./alliance-wizard.component.css']
+  styleUrls: ['./alliance-wizard.component.css'],
 })
 export class AllianceWizardComponent implements OnInit {
+  affiliationMethod: FormGroup;
+  secondFormGroup: FormGroup;
 
-  constructor() { }
+  qualifyingEvents: [] = [];
+  constructor(
+    private _formBuilder: FormBuilder,
+    private qualifyingEventService: QualifyingEventService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.qualifyingEventService.getAll().subscribe((res) => {
+      this.qualifyingEvents = <any>res;
+    });
 
+    this.affiliationMethod = this._formBuilder.group({
+      affiliationMethod: [null, [Validators.required]],
+      qualifyingEvent: [null, [Validators.required]],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: [null, [Validators.required]],
+    });
+  }
 }
