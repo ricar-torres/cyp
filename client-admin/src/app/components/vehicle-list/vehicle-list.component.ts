770ef90e503f6216facd8e3b35632597b5541482
Ricardo Torres <rtorres@innovayst.com>
2020-09-08 22:56:13 -0400

    validate ranges of year

--- client-admin/src/app/components/vehicle-list/vehicle-list.component.ts
+++ client-admin/src/app/components/vehicle-list/vehicle-list.component.ts
@@ -10,13 +10,39 @@ import {
   FormGroup,
   FormBuilder,
   Validators,
-  AbstractControl,
+  FormControl,
 } from '@angular/forms';
-import { MatSlideToggle } from '@angular/material';
-import { DependantsAPIService } from '@app/shared/dependants.api.service';
-import { AlliancesService } from '@app/shared/alliances.service';
+import {
+  MatSlideToggle,
+  DateAdapter,
+  MAT_DATE_LOCALE,
+  MAT_DATE_FORMATS,
+  MatDatepicker,
+} from '@angular/material';
 import { trigger, transition, style, animate } from '@angular/animations';
 
+import * as _moment from 'moment';
+// tslint:disable-next-line:no-duplicate-imports
+import {
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+// import { default as _rollupMoment } from 'moment';
+// const moment = _rollupMoment || _moment;
+
+// See the Moment.js docs for the meaning of these formats:
+// https://momentjs.com/docs/#/displaying/format/
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 @Component({
   selector: 'app-vehicle-list',
   templateUrl: './vehicle-list.component.html',
@@ -33,6 +59,18 @@ import { trigger, transition, style, animate } from '@angular/animations';
       ]),
     ]),
   ],
+  providers: [
+    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+    // application's root module. We provide it at the component level here, due to limitations of
+    // our example generation script.
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class VehicleListComponent implements OnInit {
   @Input() VehicleList: FormGroup[] = [];
@@ -41,6 +79,8 @@ export class VehicleListComponent implements OnInit {
   @ViewChild('dependantsEnabled', { static: true })
   dependantsEnabled: MatSlideToggle;
   typesOfRelation: Object;
+  //date = new FormControl(moment());
+
   constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
@@ -54,7 +94,7 @@ export class VehicleListComponent implements OnInit {
       model: [null, [Validators.required]],
       vin: [null, [Validators.required]],
       make: [null, [Validators.required]],
-      year: [null, [Validators.required]],
+      year: [null, [Validators.required, CustomsValidators]],
     });
 
     //newForm.get('birthDate').disable();
@@ -64,8 +104,23 @@ export class VehicleListComponent implements OnInit {
     this.VehicleList.splice(i, 1);
   }
 
-  clearIsuranceDependants(event) {
-    this.benefitChecked.emit(event);
-    if (!event) this.VehicleList = [];
-  }
+  // clearIsuranceDependants(event) {
+  //   this.benefitChecked.emit(event);
+  //   if (!event) this.VehicleList = [];
+  // }
+  // chosenYearHandler(normalizedYear: _moment.Moment) {
+  //   const ctrlValue = this.date.value;
+  //   ctrlValue.year(normalizedYear.year());
+  //   this.date.setValue(ctrlValue);
+  // }
+
+  // chosenMonthHandler(
+  //   normalizedMonth: _moment.Moment,
+  //   datepicker: MatDatepicker<_moment.Moment>
+  // ) {
+  //   const ctrlValue = this.date.value;
+  //   ctrlValue.month(normalizedMonth.month());
+  //   this.date.setValue(ctrlValue);
+  //   datepicker.close();
+  // }
 }
